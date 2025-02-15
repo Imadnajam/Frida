875b9dfa47e0ac41d37799da8856d5cbf4fd337c
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2025-02-15 13:53:46 +0100

    test

--- api/routes/hello_router.py
+++ api/routes/hello_router.py
@@ -3,6 +3,17 @@ from api.controllers.hello_controller import hello_fast_api
 
 router = APIRouter()
 
+
 @router.get("/api/py/test")
 def hello():
     return hello_fast_api()
+
+
+@router.get("/api/py/imad")
+def imad():
+    return {"message": "hhhh"}
+
+
+@router.get("/api/py/test/{name}")
+def hello_name(name: str):
+    return {"message": f"Hello {name} from FastAPI By Imad NAJAM"}
