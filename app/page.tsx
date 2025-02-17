b4732cb52c7106bc1f1ed3b16f7bfb1a12d2a74d
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2025-02-17 11:27:29 +0100

    work with  neobrutalism-styled

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import Link from "next/link";
 
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
       <h1>hello Imad</h1>
+      <Button variant="default">Click me</Button>
    </div>
   );
 }
