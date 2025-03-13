cfa8edc112840e1eff61e32008008a952648d177
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2025-03-13 11:39:25 +0000

    For production, specify exact origins instead of

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://frida2.vercel.app"],
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
