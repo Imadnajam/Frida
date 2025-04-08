e9fc17c4aae86669cab736f7d0e5f8af7439bb50
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2025-04-08 11:35:30 +0100

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,19 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from 'next-themes'
 import { Analytics } from "@vercel/analytics/react"
+import favicon from "@/public/favicon.png";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Frida",
   description: "All Files to Markdown Converter.",
+  icons: {
+    icon: [
+      { url: favicon.src, type: 'image/png' }
+    ],
+    shortcut: [{ url: favicon.src }],
+    apple: [{ url: favicon.src }],
+  },
 };
 
 export default function RootLayout({
@@ -16,8 +24,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href={favicon.src} type="image/png" />
+        <link rel="shortcut icon" href={favicon.src} type="image/png" />
+        <link rel="apple-touch-icon" href={favicon.src} />
+      </head>
       <body className={inter.className}>
-
         <ThemeProvider attribute="class" defaultTheme="system">
           {children}
           <Analytics />
